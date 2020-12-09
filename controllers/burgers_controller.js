082370b311ac8bd5d242d433e197a9cb38b4c5ea
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-09 13:35:50 -0600

    Complete skeleton for controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    
+});
+
+router.post("/api/burgers", function(req, res) {
+
+});
+
+router.put("/api/burgers/:id", function(req, res){
+
+});
+
+module.exports = router;
