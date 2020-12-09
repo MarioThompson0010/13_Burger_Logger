ace1b254915e985c13256ceda96826d120580593
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-09 13:43:31 -0600

    Complete skeleton for models

---
+++ models/burger.js
@@ -0,0 +1,17 @@
+const orm = require("../config/orm.js");
+
+let burger = {
+    all: function(cb) {
+
+    },
+    create: function(cols, vals, cb){
+
+    },
+    update: function(objColVals, condition, cb) {
+        
+    }
+
+};
+
+
+module.exports = burger;
