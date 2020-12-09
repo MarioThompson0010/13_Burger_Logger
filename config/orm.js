f5044341ffdf80420d2f7e83f5c1fc1179af7f70
Mario Thompson <MarioThompson0010@gmail.com>
2020-12-09 13:05:40 -0600

    Created skeleton for burger project

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+var connection = require("../config/connection.js");
+
+let orm = {
+    selectAll: function(tableInput, cb){
+
+    },
+
+    insertOne: function(table, cols, vals, cb){
+
+    },
+
+    updateOne: function (table, objColVals, condition, cb){
+
+    }
+
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Sloppy Burger', true);
